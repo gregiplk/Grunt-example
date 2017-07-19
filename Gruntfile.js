da7a577e6abeecc1c4cc2da48e519a6d987e8a01
Greg Izzy <izydorek.g@gmail.com>
2017-07-19 12:44:11 +0200

    update default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,13 @@ module.exports = function(grunt) {
       }
     },
 
-    imagemin: {
-    	dynamic: {
-        	files: [{
-            	expand: true,
-            	cwd: 'images/',
-           		src: ['**/*.{png,jpg,gif}'],
-            	dest: 'images/build/'
+  imagemin: {
+   	dynamic: {
+       	files: [{
+           	expand: true,
+           	cwd: 'images/',
+         		src: ['**/*.{png,jpg,gif}'],
+           	dest: 'images/build/'
         	}]
     	}
 	},
@@ -41,5 +41,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ["sass"]);
+  grunt.registerTask('default', ["sass", "imagemin", "watch"]);
 };
